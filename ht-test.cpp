11c2c298b233cdce2e082ac750a5af2d3fabdd1a
Breeze Pickford <bpickfor@usc.edu>
2024-04-25 13:50:00 -0700

    put back og hashtable test

--- ht-test.cpp
+++ ht-test.cpp
@@ -9,9 +9,56 @@
 #include <cassert>
 #include <vector>
 using namespace std;
+// ORIGINAL FILE
+int main()
+{
+    DoubleHashProber<std::string, MyStringHash> dh;
+    HashTable<
+        std::string,
+        int,
+        DoubleHashProber<std::string, MyStringHash>,
+        std::hash<std::string>,
+        std::equal_to<std::string>>
+        ht(0.7, dh);
+
+    // This is just arbitrary code. Change it to test whatever you like about your
+    // hash table implementation.
+    for (size_t i = 0; i < 10; i++)
+    {
+        std::stringstream ss;
+        ss << "hi" << i;
+        ht.insert({ss.str(), i});
+    }
+    if (ht.find("hi1") != nullptr)
+    {
+        cout << "Found hi1" << endl;
+        ht["hi1"] += 1;
+        cout << "Incremented hi1's value to: " << ht["hi1"] << endl;
+    }
+    if (ht.find("doesnotexist") == nullptr)
+    {
+        cout << "Did not find: doesnotexist" << endl;
+    }
+    cout << "HT size: " << ht.size() << endl;
+    ht.remove("hi7");
+    ht.remove("hi9");
+    cout << "HT size: " << ht.size() << endl;
+    if (ht.find("hi9") != nullptr)
+    {
+        cout << "Found hi9" << endl;
+    }
+    else
+    {
+        cout << "Did not find hi9" << endl;
+    }
+    ht.insert({"hi7", 17});
+    cout << "size: " << ht.size() << endl;
+    return 0;
+}
 
 // NEW FILE 5 - WHAT IS WRONG ALL MY TESTS PASS
 //  Test function for inserting elements and triggering resizing
+/*
 void testInsertResize()
 {
     DoubleHashProber<std::string, MyStringHash> dh;
@@ -135,6 +182,7 @@ int main()
     cout << "All tests completed successfully!" << endl;
     return 0;
 }
+*/
 
 // NEW FILE 4
 /*
@@ -427,47 +475,6 @@ int main()
     // check size and if exceed capac
     std::cout << "Hash table size after resizing: " << ht.size() << std::endl;
 
-    return 0;
-}
-
-// ORIGINAL FILE
-/*int main()
-{
-    DoubleHashProber<std::string, MyStringHash > dh;
-    HashTable<
-        std::string,
-        int,
-        DoubleHashProber<std::string, MyStringHash >,
-        std::hash<std::string>,
-        std::equal_to<std::string> > ht(0.7, dh);
-
-    // This is just arbitrary code. Change it to test whatever you like about your
-    // hash table implementation.
-    for(size_t i = 0; i < 10; i++){
-        std::stringstream ss;
-        ss << "hi" << i;
-        ht.insert({ss.str(), i});
-    }
-    if( ht.find("hi1") != nullptr ){
-        cout << "Found hi1" << endl;
-        ht["hi1"] += 1;
-        cout << "Incremented hi1's value to: " << ht["hi1"] << endl;
-    }
-    if( ht.find("doesnotexist") == nullptr ){
-        cout << "Did not find: doesnotexist" << endl;
-    }
-    cout << "HT size: " << ht.size() << endl;
-    ht.remove("hi7");
-    ht.remove("hi9");
-    cout << "HT size: " << ht.size() << endl;
-    if( ht.find("hi9") != nullptr ){
-        cout << "Found hi9" << endl;
-    }
-    else {
-        cout << "Did not find hi9" << endl;
-    }
-    ht.insert({"hi7",17});
-    cout << "size: " << ht.size() << endl;
     return 0;
 }
 */
